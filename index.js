6a246b461ed0346011de874aef7bb00c38ec364a
99sgabi <99sgabi@wp.pl>
2021-05-18 23:31:43 +0200

    metody dzialaja na miesiace tylko-> poprawie jutro

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let dataPrzyjeciaSrodkaTrwalego;
+//aktualna wartosc amortyzacji = dotyczhczasowa wartosc amortyzacji
 //12-miesiac, 4-kwartal, 1-rok
 let czestoscNaliczaniaWRoku;
 let wartoscPoczatkowa;
@@ -11,6 +12,34 @@ let wartoscNetto;
 let amortyzacjaRoczna;
 let amortyzacjaMiesieczna;
 
+const formularz = document.getElementById("formularz");
+formularz.onsubmit = (event) =>
+{
+    if(formularz.elements["wartPocz"].value == "false")
+    {
+        metodaDegresywan = false;
+        console.log(metodaLiniowa(
+            parseInt(formularz.elements["wartPocz"].value),
+            parseFloat(formularz.elements["stwaka"].value),
+            formularz.elements["data"].value,
+            parseInt(formularz.elements["czestotliwosc"].value)
+        ))
+    }
+    else
+    {
+        
+        metodaDegresywan = true;
+        console.log(metodaDegresywna(
+            parseInt(formularz.elements["wartPocz"].value),
+            parseFloat(formularz.elements["stwaka"].value),
+            parseFloat(formularz.elements["wskaznik"].value),
+            formularz.elements["data"].value,
+            parseInt(formularz.elements["czestotliwosc"].value)
+        ))
+    }
+    
+    event.preventDefault();
+}
 
 function metodaDegresywna(wartoscPoczatkowa,stawkaAmortyzacyjna,
     wskaznikAkceleracji,dataPrzyjeciaSrodkaTrwalego,czestoscNaliczaniaWRoku)
@@ -18,29 +47,40 @@ function metodaDegresywna(wartoscPoczatkowa,stawkaAmortyzacyjna,
     let aktualneUmorzenie = 0;
     let wynik = [];
     let aktualnaDataAmortyzacji = obliczStartowyDzienAmortyzacji(dataPrzyjeciaSrodkaTrwalego)
-    let degresywan = true;
+    let degresywna = true;
     while(aktualneUmorzenie < wartoscPoczatkowa)
     {
-        let amortyzacjaRoczna =  wartoscPoczatkowa*stawkaAmortyzacyjna*wskaznikAkceleracji;
-        amortyzacjaRoczna = degresywna ? amortyzacjaRoczna*wskaznikAkceleracji : amortyzacjaRoczna;
+        let amortyzacjaRoczna = 
+            degresywna ? (wartoscPoczatkowa - aktualneUmorzenie)*stawkaAmortyzacyjna*wskaznikAkceleracji : wartoscPoczatkowa*stawkaAmortyzacyjna;
+
+        if(amortyzacjaRoczna < wartoscPoczatkowa*stawkaAmortyzacyjna)
+        {
+            amortyzacjaRoczna = wartoscPoczatkowa*stawkaAmortyzacyjna
+            degresywna = false;
+        }
 
         let amortyzacjaNaliczana = amortyzacjaRoczna/czestoscNaliczaniaWRoku;
+        amortyzacjaNaliczana = Math.round(amortyzacjaNaliczana)
         let podstawaNaliczania = degresywna ? wartoscPoczatkowa - aktualneUmorzenie : wartoscPoczatkowa;
-
-        for(let i = aktualnaDataAmortyzacji.getMonth(); i < czestoscNaliczaniaWRoku && aktualneUmorzenie < wartoscPoczatkowa; 
+        console.log( amortyzacjaRoczna)
+        for(let i = aktualnaDataAmortyzacji.getMonth(); i < 12 && aktualneUmorzenie < wartoscPoczatkowa; 
             i = aktualnaDataAmortyzacji.getMonth())
         {
+            let poprzedniaWartosc = aktualneUmorzenie;
+            aktualneUmorzenie += amortyzacjaNaliczana;
             wynik.push({
                 wartoscPoczatkowa: wartoscPoczatkowa,
                 data : aktualnaDataAmortyzacji.toDateString(),
-                aktualnaWartoscAmortyzacji: aktualneUmorzenie,
+                wartoscUmorzeniaNaPoczatekOkresu: poprzedniaWartosc,
                 podstawaNaliczaniaAmortyzacji: podstawaNaliczania,
                 stawkaAmortyzacyjna: stawkaAmortyzacyjna,
-                odpisUmorzeniaWDanymOkresie: amortyzacjaNaliczana,
-                wartoscNettoNaKoniecOkresu: wartoscPoczatkowa - aktualneUmorzenie
+                odpisUmorzeniaWDanymOkresie: wartoscPoczatkowa - poprzedniaWartosc > amortyzacjaNaliczana ? 
+                                                    amortyzacjaNaliczana : wartoscPoczatkowa - poprzedniaWartosc,
+                wartoscNettoNaKoniecOkresu: wartoscPoczatkowa - aktualneUmorzenie < 0 ? 0:
+                                                    wartoscPoczatkowa - aktualneUmorzenie
             })
-            aktualnaData = nastepnyOkres(aktualnaDataAmortyzacji, czestoscNaliczaniaWRoku)
-            aktualneUmorzenie += amortyzacjaNaliczana;
+            aktualnaDataAmortyzacji = nastepnyOkres(aktualnaDataAmortyzacji, czestoscNaliczaniaWRoku)
+            if(i == 11)break;
         }
     }
 
@@ -58,26 +98,25 @@ function metodaLiniowa(wartoscPoczatkowa,stawkaAmortyzacyjna,
 
     while(aktualneUmorzenie < wartoscPoczatkowa)
     {
+        let poprzedniaWartosc = aktualneUmorzenie;
+        aktualneUmorzenie += amortyzacjaNaliczana;
         wynik.push({
             wartoscPoczatkowa: wartoscPoczatkowa,
             data : aktualnaDataAmortyzacji.toDateString(),
-            aktualnaWartoscAmortyzacji: aktualneUmorzenie,
+            wartoscUmorzeniaNaPoczatekOkresu: poprzedniaWartosc,
             podstawaNaliczaniaAmortyzacji: wartoscPoczatkowa,
             stawkaAmortyzacyjna: stawkaAmortyzacyjna,
             odpisUmorzeniaWDanymOkresie: amortyzacjaNaliczana,
             wartoscNettoNaKoniecOkresu: wartoscPoczatkowa - aktualneUmorzenie
         })
-        aktualnaData = nastepnyOkres(aktualnaDataAmortyzacji, czestoscNaliczaniaWRoku)
-        aktualneUmorzenie += amortyzacjaNaliczana;
+        aktualnaDataAmortyzacji = nastepnyOkres(aktualnaDataAmortyzacji, czestoscNaliczaniaWRoku)
     }
-
     return wynik;
 }
 
 function obliczStartowyDzienAmortyzacji(dataPrzyjeciaSrodkaTrwalego)
 {
     let dataPrzyjecia = new Date(Date.parse(dataPrzyjeciaSrodkaTrwalego));
-    let startAmortyzacji;
     let miesiac = dataPrzyjecia.getMonth();
     let rok = dataPrzyjecia.getFullYear();
     if(miesiac == 11)
@@ -92,18 +131,20 @@ function obliczStartowyDzienAmortyzacji(dataPrzyjeciaSrodkaTrwalego)
 
 function nastepnyOkres(aktualnaData, czestoscNaliczaniaWRoku)
 {
-    let aktualnaDataDoObliczenia = new Date(Date.parse(aktualnaData));
-    let miesiac = aktualnaDataDoObliczenia.getMonth();
-    let rok = aktualnaDataDoObliczenia.getFullYear();
+    let miesiac = aktualnaData.getMonth();
+    let rok = aktualnaData.getFullYear();
     let ileMiesiecy = 12/czestoscNaliczaniaWRoku;
     if(miesiac + ileMiesiecy > 11)
     {
         rok++;
         miesiac = miesiac + ileMiesiecy;
         miesiac %= 11;
-        miesiac += 1;
     }
     else
         miesiac += ileMiesiecy + 1;
-    return Date.parse(`${rok}-${miesiac}-01`);
+
+    return new Date(Date.parse(`${rok}-${miesiac}-01`));
 }
+
+
+//metodaLiniowa(120000, 0.2, "Mat 9, 2021",12);
